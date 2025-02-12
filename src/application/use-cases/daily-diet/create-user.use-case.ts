850732a4c2757184e1f3de57a5e8c806a7d48f4b
MarinhoCM <marinhoc384@gmail.com>
2025-02-12 15:11:45 -0300

    :sparkles: feat: adicionando guards para serviço

--- src/application/use-cases/daily-diet/create-user.use-case.ts
+++ src/application/use-cases/daily-diet/create-user.use-case.ts
@@ -1,10 +1,18 @@
+import { Logger } from "@nestjs/common";
 import { UseCase } from "../use-case";
 
 interface CreateUserUseCaseRequest { }
 export interface CreateUserUseCaseResponse { }
 
 export class CreateUserUseCase implements UseCase<CreateUserUseCaseRequest, CreateUserUseCaseResponse> {
+    private readonly logger = new Logger(CreateUserUseCase.name)
+
+    constructor(
+            
+    ) { }
+
     execute(request: CreateUserUseCaseRequest): Promise<CreateUserUseCaseResponse> {
+        this.logger.log(`Iniciando a criação de usuário`);
         return;
     }
 }
